7f0eb989c061f10f7cb1af9d59e28bfb03fb3078
naeem.dhattiwala <naeem.dhattiwala@brainvire.com>
2024-07-25 14:55:11 +0530

    After demo changes

--- assets/main.js
+++ assets/main.js
@@ -181,7 +181,6 @@
     $(".btn-play").click(function () {
       $videoSrc = $(this).data("src");
     });
-    console.log($videoSrc);
 
     $("#videoModal").on("shown.bs.modal", function (e) {
       $("#video").attr(
